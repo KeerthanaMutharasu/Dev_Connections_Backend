79486c400b4635df9c1ea684f72bff5448a33d8a
Keerthana <keerthana210896@gmail.com>
2026-05-28 19:11:54 +0530

    Play around with routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,35 @@ const server = express();
 
 console.log("Server restarted at", new Date());
 
-server.use("/test", (req, res) => {
-    res.send("Hello from testing!");
+// get call with query paramas
+server.get("/test", (req, res) => {
+    console.log(req.query, "query")
+    res.send("Get Testing");
 });
 
+// post call with dynamic params
+server.post("/test/:userId", (req, res) => {
+    console.log(req.params, "params")
+    res.send("Post testing!");
+});
+// put request
+server.put("/test", (req, res) => {
+    res.send("Put testing!");
+});
+
+// delete request
+server.delete("/test", (req, res) => {
+    res.send("Delete testing!");
+});
+
+// use works same for get and post
 server.use("/hello", (req, res) => {
-    res.send("Hello from hello908");
+    res.send("Hello from hello");
+});
+
+// Order matters a lot
+server.use("/", (req, res) => {
+    res.send("Home!");
 });
 
 server.listen(7777, () => {
