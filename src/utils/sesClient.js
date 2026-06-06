5e90bdd49bbecca84312a6e5df75e33085cec891
Keerthana <keerthana210896@gmail.com>
2026-06-06 23:44:18 +0530

    Removed console in ses client page.

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -2,9 +2,6 @@ const { SESClient } = require("@aws-sdk/client-ses")
 const REGION = "ap-south-1";
 
 
-console.log(process.env.AWS_SES_ACCESSKEY,
-    process.env.AWS_SES_SECRETKEY, "jhj"
-)
 const sesClient = new SESClient({
     region: REGION,
     credentials: {
